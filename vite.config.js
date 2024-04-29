7ce28db26803c5d1e65dddc6ff883b5209593af7
Jorre Heuts <heuts.jorre@gmail.com>
2024-04-29 14:40:08 +0200

    update vitconfig

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/threejs-donuts",
+  base: "/threejs-donuts/",
   root: "src/",
   publicDir: "../public/",
   build: {
